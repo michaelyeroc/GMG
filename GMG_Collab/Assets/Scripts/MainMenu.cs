96b2f336b28876cff0e9c147be4e55f9de0d962a
Corey Herington <michaelyeroc@fullsail.edu>
2019-12-19 19:49:30 -0600

    Initial project set up

---
+++ GMG_Collab/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    Dropdown m_SceneDropDown;
+
+    List<Scene> m_GameScenes;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //m_SceneDropDown.AddOptions(m_GameScenes)
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
