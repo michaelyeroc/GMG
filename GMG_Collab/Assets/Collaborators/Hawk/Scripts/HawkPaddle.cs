0dc72dc8b646b4ebda00320681073dcbababc140
uhF <stevenhf23@gmail.com>
2020-01-12 11:38:42 -0600

    Adds paddle and edge colliders
    Adds paddle and edge colliders
    to move paddle in boxy

---
+++ GMG_Collab/Assets/Collaborators/Hawk/Scripts/HawkPaddle.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+namespace Hawk
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class HawkPaddle : MonoBehaviour
+    {
+        private Rigidbody2D paddle;
+        // The paddle speed of the script in unity controls
+        // the actual paddle speed. This seems to have no effect
+        // so maybe I'm doing something wrong
+        [SerializeField]
+        private float m_PaddleSpeed = 15f;
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            paddle = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            PaddleMovement();
+        }
+
+        private void PaddleMovement()
+        {
+            Vector2 newVelocity = Vector2.zero;
+
+            if (Input.GetKey(KeyCode.LeftArrow))
+                newVelocity.x += -m_PaddleSpeed;
+            if (Input.GetKey(KeyCode.RightArrow))
+                newVelocity.x += m_PaddleSpeed;
+
+            paddle.velocity = newVelocity;
+        }
+    }
+}
