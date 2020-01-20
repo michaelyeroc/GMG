4e3414998737466e4a6286dbe826aeb69cc1af2e
uhFrost <stevenhf23@gmail.com>
2020-01-19 18:54:17 -0600

    Moves manager scripts to own folder and adds collectable manager

---
+++ GMG_Collab/Assets/Collaborators/Hawk/Scripts/Managers/HawkCollectableManager.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+namespace Hawk
+{
+    public class HawkCollectableManager : MonoBehaviour
+    {
+        #region Singleton
+        public static HawkCollectableManager Instance { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance != null)
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                Instance = this;
+            }
+        }
+        #endregion
+
+        // Start is called before the first frame update
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
