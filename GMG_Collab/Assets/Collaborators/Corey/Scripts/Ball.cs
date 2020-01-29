b121bb789bd859b57ce581dee6f0db1f4270cbe8
Corey Herington <michaelyeroc@fullsail.edu>
2020-01-28 19:58:42 -0600

    I added a trail to my ball and changed how the canvas renders. I also made the power ups spawn randomly.

--- GMG_Collab/Assets/Collaborators/Corey/Scripts/Ball.cs
+++ GMG_Collab/Assets/Collaborators/Corey/Scripts/Ball.cs
@@ -38,6 +38,8 @@ namespace Coreys_Work
             m_BallRigidBody.velocity = Vector2.zero;
             transform.position = m_StartPos;
             BallMoving = false;
+            if (GetComponent<TrailRenderer>() != null)
+                GetComponent<TrailRenderer>().Clear();
         }
 
         private void OnCollisionEnter2D(Collision2D collision)
@@ -70,6 +72,25 @@ namespace Coreys_Work
                         break;
                     case "Bottom Bounds":
                         Debug.Log("Hit Bottom");
+                        break;
+                    case "Paddle (1)":
+                        Vector3 hitPoint = contact.point;
+
+                        Vector3 paddleCenter = new Vector3(contact.collider.gameObject.transform.position.x, contact.collider.gameObject.transform.position.y);
+
+                        m_BallRigidBody.velocity = Vector2.zero;
+
+                        float difference = paddleCenter.x - hitPoint.x;
+                        
+                        if (hitPoint.x < paddleCenter.x)
+                        {
+                            // left side of paddle
+                            m_BallRigidBody.velocity = new Vector2(-Mathf.Abs(difference * m_BallSpeed), m_BallSpeed);
+                        }
+                        else
+                        {
+                            m_BallRigidBody.velocity = new Vector2(Mathf.Abs(difference * m_BallSpeed), m_BallSpeed);
+                        }
                         break;
                 }
             }
