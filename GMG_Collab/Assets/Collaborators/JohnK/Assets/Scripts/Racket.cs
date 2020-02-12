a4b29ec96186a3862755f6929ab66bfc1e8591c1
laffed <roarklaffed@gmail.com>
2020-02-12 10:45:09 -0600

    john's init commit

---
+++ GMG_Collab/Assets/Collaborators/JohnK/Assets/Scripts/Racket.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Racket : MonoBehaviour
+{
+
+    //speed
+    public float speed = 400;
+
+    void FixedUpdate()
+    {
+        //horizontal input
+        float h = Input.GetAxisRaw("Horizontal");
+
+        //Velocity
+        GetComponent<Rigidbody2D>().velocity = Vector2.right * h * speed/100;
+    }
+}
